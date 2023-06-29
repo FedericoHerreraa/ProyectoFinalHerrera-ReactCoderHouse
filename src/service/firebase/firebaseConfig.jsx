03949258270bd82a485cbb6044c9b4b59e725557
FedericoHerreraa <fede.juan.herrera@gmail.com>
2023-06-28 22:53:27 -0300

    Proyecto Final react js

--- src/service/firebase/firebaseConfig.jsx
+++ src/service/firebase/firebaseConfig.jsx
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_apiKey,
-  authDomain: process.env.VITE_authDomain,
-  projectId: process.env.VITE_projectId,
-  storageBucket: process.env.VITE_storageBucket,
-  messagingSenderId: process.env.VITE_messagingSenderId,
-  appId: process.env.VITE_appId
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
